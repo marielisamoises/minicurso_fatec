983f4b281ce2659749b42aaddcb512f3c17b1c25
marielisamoises <ma_elisa_elisa@hotmail.com>
2017-10-03 18:53:26 -0300

    c

--- projeto_minicurso/features/step_definitions/home_step.rb
+++ projeto_minicurso/features/step_definitions/home_step.rb
@@ -5,7 +5,8 @@ Dado(/^que estou na página inicial da Saraiva$/) do
   end
   
   Quando(/^digito "([^"]*)"$/) do |autor|
-    fill_in "q" 
+    input = find_field 'q'
+    input.send_keys 'Dan Brown', :enter
   end
 
   Então(/^irei ver uma lista com os livros do autor$/) do
